d698807cb9b2a9cdd3e7d66ca17d74d4c424f6fb
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-11-23 14:26:35 -0800

    Fixed About component's height and Projects data

--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -21,7 +21,7 @@ export const projects = [
     {
         name: 'HungryHub',
         images: [process.env.PUBLIC_URL + '/hungryhub1.png', process.env.PUBLIC_URL + '/hungryhub2.png', process.env.PUBLIC_URL + '/hungryhub3.png',],
-        type: 'Food Delivery',
+        type: 'Food Delivery App',
         description: 'HungryHub is an app helping users find restaurants and get their food delivered. Built with React, AWS Amplify, AWS Cognito, DynamoDB and Material UI.',
         url: 'https://www.hungryhub.us',
         github: 'https://github.com/Jehman06/HungryHub.git'
@@ -29,7 +29,7 @@ export const projects = [
     {
         name: 'Fit by Me',
         images: [process.env.PUBLIC_URL + '/Fit-by-Me.png',],
-        type: 'MERN',
+        type: 'MERN Stack Fitness App',
         description: 'Fit by Me is a fitness and diet tracker. Find thousands of exercises and recipes and track them in one place. Built with React, Node, MongoDB, Express and Material UI.',
         url: 'https://github.com/sswift1995/myfitness_app.git',
         github: 'https://github.com/sswift1995/myfitness_app.git'
